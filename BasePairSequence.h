54c17a05225e9e2bf6cd0a2539b69cfe194a0a4a
Shubo Yang <yangshubo@outlook.com>
2015-08-27 14:26:23 +0800

    add getter to BasePairSequence

--- BasePairSequence.h
+++ BasePairSequence.h
@@ -20,22 +20,32 @@ public:
 	
 	// TODO non-trivial varialbes  
 	void Init();
-	
 	//generates the sequence for the complementary strand
 	void ReverseComplement();
-	  
-	//sequence for quality
-	void* qual;
 	
-	//sequence based on As, Cs, Gs, Ts and Ns
-	void* original_basepair;
+	//getter for sequence for quality 
+	void const* qual() { return qual_; }
+	//getter for sequence based on As, Cs, Gs, Ts and Ns
+	void const* original_basepair() { return original_basepair_; }
+	size_t original_basepair_size() { return original_basepair_size_; }
+	//getter for compressed in the same way to Hash table
+	unsigned int const* codes() { return codes_; }
+	size_t codes_size(){ return codes_size_; }
 
-	size_t original_basepair_size;
+private:
+
+	//sequence for quality
+	void* qual_;
+
+	//sequence based on As, Cs, Gs, Ts and Ns
+	void* original_basepair_;
+
+	size_t original_basepair_size_;
 
 	//compressed in the same way to Hash table
-	unsigned int * codes;
+	unsigned int * codes_;
 
-	size_t codes_size;
+	size_t codes_size_;
 };
 
 #endif //STELLA_BASE_PAIR_H_
