ecffc3e83db9f0c45799572ce55c7ff321e98e22
bigcatroyaly <royalyk@126.com>
2015-08-28 23:15:20 +0800

    encode now cover whole situations

--- BasePairSequence.cpp
+++ BasePairSequence.cpp
@@ -1,7 +1,10 @@
 #include "BasePairSequence.h"
 #include <string>
+#include <iostream>
 #include "Stella.h"
 #include "BasePairCodeSample.h"
+
+//#define DEBUG_ONLY
 
 using namespace std;
 
@@ -45,15 +48,14 @@ void BasePairSequence::ReverseComplement()
 
 HResult BasePairSequence::ReadCode(BasePairCodeSample * CurrentCodes)
 {
-    if ( original_basepair_size_ < 
-             offset_ + MAX_BP_LEN * (this->original_basepair_size_ / MAX_BP_LEN)
-       )
+    if ( offset_ >= MAX_BP_LEN )
     {
         return HResult::kFAIL;
     }
     
-    this->codes_len_ = this->original_basepair_size_ / MAX_BP_LEN;
+    this->codes_len_ = (this->original_basepair_size_-offset_) / MAX_BP_LEN;
     this->codes_ = new unsigned int[this->codes_len_];
+    
 
     // Makes the codes base on the genome sequence
     int codes_i = 0;
@@ -61,17 +63,41 @@ HResult BasePairSequence::ReadCode(BasePairCodeSample * CurrentCodes)
     codes_[codes_i] = 0;
     string * squence = static_cast<string *> (original_basepair_);
 
+#ifdef DEBUG_ONLY
+    cout <<"CodeSample Now is:" <<endl;
+    cout << "offset is " << offset_ << " has " << codes_len_<<" codes , encode "
+        << codes_len_ * MAX_BP_LEN <<" bp"<<endl;
+#endif
     for (size_t i = offset_; i < original_basepair_size_; i++)
     {
+#ifdef DEBUG_ONLY
+        cout << (*squence)[i];
+#endif
         codes_[codes_i] += BitCodes[(*squence)[i] - 'A'] << (code_basepair_num * CODE_LEN);
+
+        code_basepair_num++;
+
         if (code_basepair_num == MAX_BP_LEN)
         {
-            codes_i++;
+
+#ifdef DEBUG_ONLY
+            cout << endl;
+            cout <<"code number "<< codes_i << "  finished" << endl;
+#endif
+            
+            codes_i++;        
             codes_[codes_i] = 0;
             code_basepair_num = 0;
+            if (codes_len_ == codes_i)
+                break;
         }
+    
     }
     
+#ifdef DEBUG_ONLY
+    cout << endl;
+#endif
+
     //TODO make the codes base on the genome sequence
     CurrentCodes->SetData(this->codes_, codes_len_, offset_);
     offset_++;
